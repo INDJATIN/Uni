c5d33a90fe629334c0c2048beeefc0f294d23a85
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-17 02:08:56 +0530

    Update universal.py

--- Bypass/universal.py
+++ Bypass/universal.py
@@ -1,7 +1,10 @@
 from Bypass.core.sites.toonshub import toonshub_main
 
-async def universal(url):
+async def universal(url, message):
+     reply = await sendMessage(message, "Bypassing....")
+     result = ""
      if "toonshub" in url:
-         return await toonshub_main(url)
+         result += await toonshub_main(url)
      else:
          return
+     await editMessage(reply, result)
