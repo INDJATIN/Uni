49be9819cbaf1ded67729555946f2c2f9f5811b9
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 18:01:14 +0530

    Update __main__.py

--- Bypass/__main__.py
+++ Bypass/__main__.py
@@ -1,14 +1,40 @@
-from pyrogram import idle
+from pyrogram import idle, Client
+from pyrogram.enums import ParseMode
 from pyrogram.filters import command, user
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from os import path as ospath, execl
+from time import time
+from dotenv import load_dotenv
+from os import path as ospath, execl, getenv
 from asyncio import create_subprocess_exec
 from sys import executable
+from logging import getLogger, FileHandler, StreamHandler, INFO, ERROR, basicConfig
+from uvloop import install
+
 from Bypass.core.sites.gdtot_bypasser import gdtot
 
-bot = Client("Diffusion", api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN, plugins=dict(root="Bypass/plugins"), parse_mode=ParseMode.HTML)
+install()
+basicConfig(format="[%(asctime)s] [%(levelname)s] - %(message)s", #  [%(filename)s:%(lineno)d]
+            datefmt="%d-%b-%y %I:%M:%S %p",
+            handlers=[FileHandler('log.txt'), StreamHandler()],
+            level=INFO)
+
+getLogger("pyrogram").setLevel(ERROR)
 LOGGER = getLogger(__name__)
+
+load_dotenv('config.env', override=True)
 BOT_START = time()
+
+class Config:
+    BOT_TOKEN = getenv('BOT_TOKEN', '')
+    API_HASH = getenv('API_HASH', '')
+    API_ID = getenv('API_ID', '')
+    if BOT_TOKEN == '' or API_HASH == '' or API_ID == '':
+        LOGGER.critical('ENV Missing. Exiting Now...')
+        exit(1)
+    OWNER_ID = getenv('OWNER_ID','')
+    GDTOT_CRYPT = getenv('GDTOT_CRYPT', '')
+    
+bot = Client("Diffusion", api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN, plugins=dict(root="Bypass/plugins"), parse_mode=ParseMode.HTML)
 
 @bot.on_message(filters.command('start'))
 async def start_command(client, message):
