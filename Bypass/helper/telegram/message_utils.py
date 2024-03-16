e55f72432a680f9c4a862ba0c3aca2a00571d5bc
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 17:09:03 +0530

    Create message_utils.py

---
+++ Bypass/helper/telegram/message_utils.py
@@ -0,0 +1,48 @@
+from pyrogram.errors import ReplyMarkupInvalid, FloodWait, PeerIdInvalid, ChannelInvalid, RPCError, UserNotParticipant, MessageNotModified, MessageEmpty, PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty
+from Bypass.helper.telegram_helper.button_build import ButtonMaker
+
+async def chat_info(channel_id):
+    channel_id = str(channel_id).strip()
+    if channel_id.startswith('-100'):
+        channel_id = int(channel_id)
+    elif channel_id.startswith('@'):
+        channel_id = channel_id.replace('@', '')
+    else:
+        return None
+    try:
+        return await bot.get_chat(channel_id)
+    except (PeerIdInvalid, ChannelInvalid) as e:
+        LOGGER.error(f"{e.NAME}: {e.MESSAGE} for {channel_id}")
+        return None
+
+  async def forcesub(message, ids, button=None):
+    join_button = {}
+    _msg = ''
+    for channel_id in ids.split():
+        chat = await chat_info(channel_id)
+        try:
+            await chat.get_member(message.from_user.id)
+        except UserNotParticipant:
+            if username := chat.username:
+                invite_link = f"https://t.me/{username}"
+            else:
+                invite_link = chat.invite_link
+            join_button[chat.title] = invite_link
+        except RPCError as e:
+            LOGGER.error(f"{e.NAME}: {e.MESSAGE} for {channel_id}")
+        except Exception as e:
+            LOGGER.error(f'{e} for {channel_id}')
+    if join_button:
+        if button is None:
+            button = ButtonMaker()
+        _msg = "You haven't joined our channel yet!"
+        for key, value in join_button.items():
+            button.ubutton(f'Join {key}', value, 'footer')
+    return _msg, button
+
+
+async def user_info(user_id):
+    try:
+        return await bot.get_users(user_id)
+    except Exception:
+        return ''
