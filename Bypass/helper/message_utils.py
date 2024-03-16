7369a143213bbacdce978fbbfc8e51dbbfc2317f
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 23:04:39 +0530

    Update message_utils.py

--- Bypass/helper/message_utils.py
+++ Bypass/helper/message_utils.py
@@ -1,5 +1,13 @@
-from pyrogram.errors import ReplyMarkupInvalid, FloodWait, PeerIdInvalid, ChannelInvalid, RPCError, UserNotParticipant, MessageNotModified, MessageEmpty, PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty
+from traceback import format_exc
+from asyncio import sleep
+from aiofiles.os import remove as aioremove
+from random import choice as rchoice
+from time import time
+from re import match as re_match
+from Bypass import LOGGER, bot
 from Bypass.helper.button_build import ButtonMaker
+from pyrogram.types import InputMediaPhoto
+from pyrogram.errors import ReplyMarkupInvalid, FloodWait, PeerIdInvalid, ChannelInvalid, RPCError, UserNotParticipant, MessageNotModified, MessageEmpty, PhotoInvalidDimensions, WebpageCurlFailed, MediaEmptyfrom Bypass.helper.button_build import ButtonMaker
 
 async def chat_info(channel_id):
     channel_id = str(channel_id).strip()
@@ -14,8 +22,64 @@ async def chat_info(channel_id):
     except (PeerIdInvalid, ChannelInvalid) as e:
         LOGGER.error(f"{e.NAME}: {e.MESSAGE} for {channel_id}")
         return None
+        
+async def deleteMessage(message):
+    try:
+        await message.delete()
+    except Exception as e:
+        LOGGER.error(str(e))
+        
+async def editMessage(message, text, buttons=None, photo=None):
+    try:
+        if message.media:
+            if photo:
+                photo = rchoice(config_dict['IMAGES']) if photo == 'IMAGES' else photo
+                return await message.edit_media(InputMediaPhoto(photo, text), reply_markup=buttons)
+            return await message.edit_caption(caption=text, reply_markup=buttons)
+        await message.edit(text=text, disable_web_page_preview=True, reply_markup=buttons)
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        await sleep(f.value * 1.2)
+        return await editMessage(message, text, buttons, photo)
+    except (MessageNotModified, MessageEmpty):
+        pass
+    except ReplyMarkupInvalid:
+        return await editMessage(message, text, None, photo)
+    except Exception as e:
+        LOGGER.error(str(e))
+        return str(e)
+        
+async def sendMessage(message, text, buttons=None, photo=None):
+    try:
+        if photo:
+            try:
+                if photo == 'IMAGES':
+                    photo = rchoice(config_dict['IMAGES'])
+                return await message.reply_photo(photo=photo, reply_to_message_id=message.id,
+                                                 caption=text, reply_markup=buttons, disable_notification=True)
+            except IndexError:
+                pass
+            except (PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty):
+                des_dir = await download_image_url(photo)
+                await sendMessage(message, text, buttons, des_dir)
+                await aioremove(des_dir)
+                return
+            except Exception as e:
+                LOGGER.error(format_exc())
+        return await message.reply(text=text, quote=True, disable_web_page_preview=True,
+                                   disable_notification=True, reply_markup=buttons)
+    except FloodWait as f:
+        LOGGER.warning(str(f))
+        await sleep(f.value * 1.2)
+        return await sendMessage(message, text, buttons, photo)
+    except ReplyMarkupInvalid:
+        return await sendMessage(message, text, None, photo)
+    except Exception as e:
+        LOGGER.error(format_exc())
+        return str(e)
 
-  async def forcesub(message, ids, button=None):
+
+async def forcesub(message, ids, button=None):
     join_button = {}
     _msg = ''
     for channel_id in ids.split():
