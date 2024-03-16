b40a9a1a4b36976061e3c96747dded1d3c12a01b
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 16:32:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-from re import compile, search, match
+import asyncio
 from pyrogram import Client, filters
-from gdtot_bypasser import gdtot
-from logging import getLogger
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from gdtot_bypaser import gdtot
 
 bot = Client(
     "project",
@@ -12,21 +9,37 @@ bot = Client(
     api_hash="661ce2469998288dbac1147e10ff4d44",
     bot_token="6715308533:AAEVkp7wqYv0fwTiJdZGrRAXt1ygMsxqg-s",
 )
-LOGGER = getLogger(__name__)
+
+CHANNEL_ID = "gdtot_bypasser"
+
 
 @bot.on_message(filters.command('start'))
 async def start_command(client, message):
     await message.reply("Hey, I am Gdtot Bypasser Bot, Just Send Your Gdtot Links And Get Drive Links")
 
+
 @bot.on_message(filters.regex(r'https?://\S+'))
 async def scrape_data(client, message):
     if "gdtot" in message.text:
-        reply = await message.reply("Bypassing")
-        link = message.text
-        result = await gdtot(link)
-        await reply.edit(result)
+        if await user_joined_channel(client, message):
+            reply = await message.reply("Bypassing")
+            link = message.text
+            result = await gdtot(link)
+            await reply.edit(result)
     elif "gdtot" not in message.text:
-        await message.reply("Sorry , Bot Support Only Gdtot Links")
+        await message.reply("Sorry, Bot Supports Only Gdtot Links")
 
-LOGGER.info("Bot Started")
+
+async def user_joined_channel(client, message):
+    if not await client.get_chat_member(CHANNEL_ID, message.from_user.id):
+        await message.reply(
+            "Please join our channel to access this command.",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("Join Channel", url=f"https://t.me/{CHANNEL_ID}")]]
+            )
+        )
+        return False
+    return True
+
+
 bot.run()
