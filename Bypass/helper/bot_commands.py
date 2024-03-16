de4159f86273389454bbe5907bf383f7b72036af
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 21:20:15 +0530

    Update bot_commands.py

--- Bypass/helper/bot_commands.py
+++ Bypass/helper/bot_commands.py
@@ -1,6 +1,5 @@
 class _BotCommands:
     def __init__(self):
-    self.StartCommand = 'start'
-    self.RestartCommand = 'restart'
-
+        self.StartCommand = 'start'
+        self.RestartCommand = 'restart'
 BotCommands = _BotCommands()
