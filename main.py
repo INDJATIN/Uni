c76901b86ba6619f4e72098e55b36c3c78085b00
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 15:15:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 from re import compile, search, match
 from pyrogram import Client, filters
 from gdtot_bypasser import gdtot
+from logging import getlogging
 
 bot = Client(
     "project",
@@ -11,19 +12,21 @@ bot = Client(
     api_hash="661ce2469998288dbac1147e10ff4d44",
     bot_token="6531755753:AAHGBmBD6-eiHZgPvmZjeGxG1Ge4-HKYE2E",
 )
+LOGGER = getlogging(__name__)
 
 @bot.on_message(filters.command('start'))
 async def start_command(client, message):
-    await message.reply("Hey, I am Gdtot Bypasser Bot, just Send your Gdtot Links and get drive links")
+    await message.reply_text("Hey, I am Gdtot Bypasser Bot, Just Send Your Gdtot Links And Get Drive Links")
 
 @bot.on_message(filters.regex(r'https?://\S+'))
 async def scrape_data(client, message):
     if "gdtot" in message.text:
-        reply = await message.reply("Bypassing In 20 Seconds")
+        reply = await message.reply_text("Bypassing In 20 Seconds")
         link = message.text
         result = await gdtot(link)
-        await reply.reply(result)
+        await reply.reply_text(result)
     elif "gdtot" not in message.text:
-        await message.reply("Sorry , Bot Support Only Gdtot Links")
+        await message.reply_text("Sorry , Bot Support Only Gdtot Links")
 
 bot.run()
+LOGGER.info("Bot Started")
