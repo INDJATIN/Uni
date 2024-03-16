811b40f2b49bff0c6dec9cbc3734cbacf8527e33
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-17 01:52:37 +0530

    Create toonshub.py

---
+++ Bypass/core/sites/toonshub.py
@@ -0,0 +1,20 @@
+import requests
+from re import search
+from bs4 import BeautifulSoup
+
+async def tooshub_main(url):
+    if search(r'.*episode.*', url):
+        return
+    elif search(r'.*redirect.*', url):
+        return
+    else:
+        return await toonshub_scraper(url)
+      
+async def toonshub_scraper(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text,'html.parser')
+    sub = soup.find_all('a',{'class':'shortc-button medium button'})
+    for s in sub:
+        item = s['href'].split('/')
+        msg += f"<b>{item[4]} {item[3]} {item[-1]}</b>\n {s['href']}\n\n"
+    return msg
