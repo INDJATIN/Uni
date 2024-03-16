7c50a987a3ceac8b4525c2ea1ef347509215cfcf
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-16 18:23:13 +0530

    Update __inti__.py

--- Bypass/__inti__.py
+++ Bypass/__inti__.py
@@ -1,1 +1,31 @@
+from os import getenv
+from time import time
+from dotenv import load_dotenv
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+from logging import getLogger, FileHandler, StreamHandler, INFO, ERROR, basicConfig
+from uvloop import install
 
+install()
+basicConfig(format="[%(asctime)s] [%(levelname)s] - %(message)s", #  [%(filename)s:%(lineno)d]
+            datefmt="%d-%b-%y %I:%M:%S %p",
+            handlers=[FileHandler('log.txt'), StreamHandler()],
+            level=INFO)
+
+getLogger("pyrogram").setLevel(ERROR)
+LOGGER = getLogger(__name__)
+
+load_dotenv('config.env', override=True)
+BOT_START = time()
+
+class Config:
+    BOT_TOKEN = getenv('BOT_TOKEN', '')
+    API_HASH = getenv('API_HASH', '')
+    API_ID = getenv('API_ID', '')
+    if BOT_TOKEN == '' or API_HASH == '' or API_ID == '':
+        LOGGER.critical('ENV Missing. Exiting Now...')
+        exit(1)
+    OWNER_ID = getenv('OWNER_ID','')
+    GDTOT_CRYPT = getenv('GDTOT_CRYPT', '')
+    
+bot = Client("Diffusion", api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN, plugins=dict(root="Bypass/plugins"), parse_mode=ParseMode.HTML)
