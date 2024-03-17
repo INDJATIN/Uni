23c31780878df031b583c58f830a922720fb7981
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-17 08:03:24 +0530

    Update universal.py

--- Bypass/universal.py
+++ Bypass/universal.py
@@ -1,11 +1,18 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Bypass.core.sites.toonshub import toonshub_main
 from Bypass.helper.message_utils import editMessage, sendMessage, deleteMessage
 
 async def universal(url, message):
-     reply = await sendMessage(message, "Bypassing....")
-     result = ""
-     if "toonshub" in url:
-         result += await toonshub_main(url)
-     else:
-         return
-     await editMessage(reply, result)
+    reply = await sendMessage(message, "Bypassing....")
+    result = ""
+    if "toonshub" in url:
+        result += await toonshub_main(url)
+    else:
+        return "No Bypass Found"
+    await reply.edit_text(result,
+                          quote=True,
+                          reply_markup=InlineKeyboardMarkup([
+                               [InlineKeyboardButton('🎓 Support Group', url='https://t.me/uni_bypasser')]
+                          ])
+                         )
+
