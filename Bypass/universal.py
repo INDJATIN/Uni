1301a5d26d539e9f7423c82a84f1345f44704c70
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-03-17 09:37:38 +0530

    Update universal.py

--- Bypass/universal.py
+++ Bypass/universal.py
@@ -2,6 +2,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from Bypass.core.sites.toonshub import toonshub_main
 from Bypass.core.sites.kayoanime import kayoanime
+from Bypass.core.production.ads_scraper import cloudbypass
 from Bypass.helper.message_utils import editMessage, sendMessage, deleteMessage
 
 async def universal(url, message):
@@ -12,6 +13,9 @@ async def universal(url, message):
     elif "kayoanime" in url:
         reply = await sendMessage(message, "Getting Contents and Details")
         result += await kayoanime(url)
+    elif "" in url:
+        reply = await sendMessage(message, "Bypassing Ads...")
+        result += await cloudbypass(url)
     else:
         return "No Bypass Found"
     await reply.edit_text(result,
